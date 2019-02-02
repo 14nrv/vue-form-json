3a5f5f5e8e5fdb94b37a0b5fffd4491b125349a0
14nrv <contact@styro.fr>
2019-02-02 17:15:43 +0100

    feat: allow custom css classes

--- src/components/Form/Form.spec.js
+++ src/components/Form/Form.spec.js
@@ -194,6 +194,68 @@ describe('Form', () => {
     b.domHasNot($errorMessage)
   })
 
+  describe('custom css class', () => {
+    it('apply custom classes', () => {
+      const customClassInJson = flatten(fields).find(x => Object.keys(x).includes('parentClass')).parentClass
+      const allCustomClass = wrapper.findAll(`.field.${customClassInJson.split(' ').join('.')}`)
+
+      expect(allCustomClass).toHaveLength(1)
+    })
+
+    it('add customs class in json array', async () => {
+      const Y_CLASS_NAME = 'yClass'
+      const Z_CLASS_NAME = 'zClass'
+
+      wrapper.setProps({
+        formFields: [
+          [
+            { label: 'x' },
+            { label: 'y', parentClass: Y_CLASS_NAME },
+            { label: 'z', parentClass: Z_CLASS_NAME }
+          ]
+        ]
+      })
+      await wrapper.vm.$nextTick()
+
+      b.domHas(`.field.${Y_CLASS_NAME}`)
+      b.domHas(`.field.${Z_CLASS_NAME}`)
+
+      const allY = wrapper.findAll(`.${Y_CLASS_NAME}`)
+      expect(allY).toHaveLength(1)
+
+      const allZ = wrapper.findAll(`.${Z_CLASS_NAME}`)
+      expect(allZ).toHaveLength(1)
+    })
+
+    it('add custom class with classic json', async () => {
+      const LABEL_CLASS = 'labelClass'
+
+      wrapper.setProps({ formFields: [{ label: 'label', parentClass: LABEL_CLASS }] })
+      await wrapper.vm.$nextTick()
+
+      b.domHas(`.field.${LABEL_CLASS}`)
+    })
+
+    it('add custom class with json slot', async () => {
+      const SLOT_CLASS = 'slotClass'
+
+      wrapper.setProps({ formFields: [{ slot: 'slotName', parentClass: SLOT_CLASS }] })
+      await wrapper.vm.$nextTick()
+
+      b.domHas(`.field.${SLOT_CLASS}`)
+    })
+
+    it('add custom class with json html content', async () => {
+      const HTML_CLASS = 'htmlClass'
+      const CONTENT_CLASS = 'custom-content'
+
+      wrapper.setProps({ formFields: [{ html: `<p class=${CONTENT_CLASS}>content</p>`, parentClass: HTML_CLASS }] })
+      await wrapper.vm.$nextTick()
+
+      b.domHas(`.field.${HTML_CLASS} > .${CONTENT_CLASS}`)
+    })
+  })
+
   describe('slot', () => {
     const slotContainer = '[data-test=slot]'
 
